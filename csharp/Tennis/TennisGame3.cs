f2581c2d0d575a0cca12cd7ac0337ed266ceaa8d
MATHIEU Alexis <alexis.mathieu@citeo.com>
2024-09-27 11:26:13 +0200

    refactoring : rename player name refactoring rename score player refactoring create new var points refactoring replace condition to var

--- csharp/Tennis/TennisGame3.cs
+++ csharp/Tennis/TennisGame3.cs
@@ -2,41 +2,40 @@ namespace Tennis
 {
     public class TennisGame3 : ITennisGame
     {
-        private int p2;
-        private int p1;
-        private string p1N;
-        private string p2N;
+        private int player2Score;
+        private int player1Score;
+        private string player1Name;
+        private string player2Name;
+        private readonly string[] points = { "Love", "Fifteen", "Thirty", "Forty" };
 
         public TennisGame3(string player1Name, string player2Name)
         {
-            this.p1N = player1Name;
-            this.p2N = player2Name;
+            this.player1Name = player1Name;
+            this.player2Name = player2Name;
         }
 
         public string GetScore()
         {
-            string s;
-            if ((p1 < 4 && p2 < 4) && (p1 + p2 < 6))
+            string score;
+            bool isBeforeDeuceOrWin = (player1Score < 4 && player2Score < 4) && (player1Score + player2Score < 6);
+            if (isBeforeDeuceOrWin)
             {
-                string[] p = { "Love", "Fifteen", "Thirty", "Forty" };
-                s = p[p1];
-                return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
+                score = points[player1Score];
+                return (player1Score == player2Score) ? score + "-All" : score + "-" + points[player2Score];
             }
-            else
-            {
-                if (p1 == p2)
-                    return "Deuce";
-                s = p1 > p2 ? p1N : p2N;
-                return ((p1 - p2) * (p1 - p2) == 1) ? "Advantage " + s : "Win for " + s;
-            }
+
+            if (player1Score == player2Score)
+                return "Deuce";
+            score = player1Score > player2Score ? player1Name : player2Name;
+            return ((player1Score - player2Score) * (player1Score - player2Score) == 1) ? "Advantage " + score : "Win for " + score;
         }
 
         public void WonPoint(string playerName)
         {
             if (playerName == "player1")
-                this.p1 += 1;
+                this.player1Score += 1;
             else
-                this.p2 += 1;
+                this.player2Score += 1;
         }
 
     }
